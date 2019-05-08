e2ec0eca254b0063dce7b2b5baa0b957f6787037
Serge NOEL <serge.noel@net6a.com>
2019-05-08 22:46:04 +0200

    Ajout calendrier

--- www/ajax/feed.php
+++ www/ajax/feed.php
@@ -1,20 +1,27 @@
 <?php
-require __DIR__ . '/vendor/autoload.php';
-
-/*if (php_sapi_name() != 'cli') {
-    throw new Exception('This application must be run on the command line.');
-}*/
+/**
+ * Récupère les données pour le mois actif
+ * php version 7.2
+ * 
+ * @category REST
+ * @package  Project
+ * @author   Serge NOEL <serge.noel@easylinux.fr>
+ * @license  GNU GPL
+ * @link     http://www.easylinux.fr/calendar
+ */
+require '../vendor/autoload.php';
 
 /**
  * Returns an authorized API client.
- * @return Google_Client the authorized client object
+ * 
+ * @return object Google_Client the authorized client object
  */
 function getClient()
 {
     $client = new Google_Client();
-    $client->setApplicationName('Google Calendar API PHP Quickstart');
+    $client->setApplicationName('Easy Calendar');
     $client->setScopes(Google_Service_Calendar::CALENDAR_READONLY);
-    $client->setAuthConfig('config/credentials.json');
+    $client->setAuthConfig('../config/credentials.json');
     $client->setAccessType('offline');
     $client->setPrompt('select_account consent');
 
@@ -22,7 +29,7 @@ function getClient()
     // The file token.json stores the user's access and refresh tokens, and is
     // created automatically when the authorization flow completes for the first
     // time.
-    $tokenPath = 'config/token.json';
+    $tokenPath = '../config/token.json';
     if (file_exists($tokenPath)) {
         $accessToken = json_decode(file_get_contents($tokenPath), true);
         $client->setAccessToken($accessToken);
@@ -63,79 +70,29 @@ function getClient()
 $client = getClient();
 $service = new Google_Service_Calendar($client);
 
-$results = $service->calendarList->listCalendarList();
-
-//echo nl2br(print_r($results->getItems(), true));
-foreach($results->getItems() as $result)
-{
-  echo $result->getSummary(). "<br/>\n";
-}
-echo "OK";
-// Print the next 10 events on the user's calendar.
 $calendarId = 'sujvsvr51d6ueir37kd8ntm2ng@group.calendar.google.com';
 $optParams = array(
   'maxResults' => 100,
   'orderBy' => 'startTime',
   'singleEvents' => true,
-  'timeMin' => date('c'),
-  'timeMax' => date('c')
+  'timeMin' => $_POST["start"],
+  'timeMax' => $_POST["end"]
 );
 $results = $service->events->listEvents($calendarId, $optParams);
 $events = $results->getItems();
 
+
 if (empty($events)) {
-    print "No upcoming events found.<br />\n";
+    $oEvents=[];
 } else {
-    print "Upcoming events:<br />\n";
     foreach ($events as $event) {
-        $start = $event->start->dateTime;
-        if (empty($start)) {
-            $start = $event->start->date;
-        }
-        printf("%s (%s-%s)<br />\n", $event->getSummary(), $start, $event->getId());
+        $oEvents[]=[
+          'id' => $event->getId(),
+          'title' => $event->getSummary(),
+          'start' => $event->start->dateTime
+        ];
     }
 }
 
+echo json_encode($oEvents);
 
-//$event = $service->events->get('primary', "eventId");
-//echo $event->getSummary();
-//echo nl2brfile_get_contents("templates/calview.tmpl");
-
-
-// Refer to the PHP quickstart on how to setup the environment:
-// https://developers.google.com/calendar/quickstart/php
-// Change the scope to Google_Service_Calendar::CALENDAR and delete any stored
-// credentials.
-/*
-$event = new Google_Service_Calendar_Event(array(
-    'summary' => 'Google I/O 2015',
-    'location' => '800 Howard St., San Francisco, CA 94103',
-    'description' => 'A chance to hear more about Google\'s developer products.',
-    'start' => array(
-      'dateTime' => '2015-05-28T09:00:00-07:00',
-      'timeZone' => 'America/Los_Angeles',
-    ),
-    'end' => array(
-      'dateTime' => '2015-05-28T17:00:00-07:00',
-      'timeZone' => 'America/Los_Angeles',
-    ),
-    'recurrence' => array(
-      'RRULE:FREQ=DAILY;COUNT=2'
-    ),
-    'attendees' => array(
-      array('email' => 'lpage@example.com'),
-      array('email' => 'sbrin@example.com'),
-    ),
-    'reminders' => array(
-      'useDefault' => FALSE,
-      'overrides' => array(
-        array('method' => 'email', 'minutes' => 24 * 60),
-        array('method' => 'popup', 'minutes' => 10),
-      ),
-    ),
-  ));
-  
-  $calendarId = 'primary';
-  $event = $service->events->insert($calendarId, $event);
-  printf('Event created: %s\n', $event->htmlLink);
-  */
--- www/js/menu.js
+++ www/js/menu.js
@@ -1,9 +1,139 @@
-$("#Logout").click(function() {
-    console.log("Logout");
-    $.post("ajax/menu.php", "Action=Logout", function(data, status) {
-        console.log("Data: " + data + "\nStatus: " + status);
-        if (data == "OUT") {
-            location.reload();
+// ApiKey : AIzaSyCF_2be1DdAKXdTsG0U30K-SHL3aW32cJY
+// URL : https://calendar.google.com/calendar?cid=c3VqdnN2cjUxZDZ1ZWlyMzdrZDhudG0ybmdAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ
+
+
+
+/*
+$(document).ready(function() {
+    var calendarEl = $('#calendar');
+
+
+*/
+
+/*    var calendar = new FullCalendar.Calendar(calendarEl, {
+        navLinks: true, // can click day/week names to navigate views
+        plugins: ['dayGrid', 'list', 'googleCalendar', 'interaction'],
+        eventLimit: true, // allow "more" link when too many events
+        events: [{
+                title: 'All Day Event',
+                start: '2019-05-01'
+            },
+            {
+                title: 'Long Event',
+                start: '2019-05-07',
+                end: '2019-05-10'
+            },
+            {
+                id: 999,
+                title: 'Repeating Event',
+                start: '2019-05-09T16:00:00'
+            },
+            {
+                id: 999,
+                title: 'Repeating Event',
+                start: '2019-05-16T16:00:00'
+            },
+            {
+                title: 'Conference',
+                start: '2019-05-11',
+                end: '2019-05-13'
+            },
+            {
+                title: 'Meeting',
+                start: '2019-05-12T10:30:00',
+                end: '2019-05-12T12:30:00'
+            },
+            {
+                title: 'Lunch',
+                start: '2019-05-12T12:00:00'
+            },
+            {
+                title: 'Meeting',
+                start: '2019-05-12T14:30:00'
+            },
+            {
+                title: 'Happy Hour',
+                start: '2019-05-12T17:30:00'
+            },
+            {
+                title: 'Dinner',
+                start: '2019-05-12T20:00:00'
+            },
+            {
+                title: 'Birthday Party',
+                start: '2019-05-12T07:00:00'
+            },
+            {
+                title: 'Click for Google',
+                url: 'https://google.com/',
+                start: '2019-05-28'
+            }
+        ],
+
+*/
+
+$(document).ready(function() {
+    //document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendar');
+
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+        plugins: ['dayGrid', 'list', 'googleCalendar', 'interaction', 'timeGrid'],
+        header: {
+            left: 'prev, next today',
+            center: 'title',
+            right: 'month,basicWeek,basicDay,dayGridMonth,listYear'
+        },
+        eventLimit: true, // allow "more" link when too many events
+        //googleCalendarApiKey: 'AIzaSyCF_2be1DdAKXdTsG0U30K-SHL3aW32cJY',
+        //events: {
+        //    googleCalendarId: 'sujvsvr51d6ueir37kd8ntm2ng@group.calendar.google.com'
+        //},
+        events: {
+            url: 'ajax/feed.php',
+            method: 'POST',
+        },
+        buttonText: {
+            today: 'Aujourd\'hui',
+            month: 'Mois',
+            week: 'Semaine',
+            day: 'Jour',
+            list: 'Liste'
+        },
+        //defaultView: 'listMonth',
+        locale: 'fr',
+        selectable: true,
+        editable: true,
+        header: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay'
+        },
+        dateClick: function(info) {
+            console.log("DateClick:");
+            console.log('Current view: ' + info.view.type);
+            console.log(info);
+            console.log('clicked ' + info.dateStr);
+        },
+        select: function(info) {
+            console.log(info);
+            console.log('selected ' + info.startStr + ' to ' + info.endStr);
+        },
+        eventClick: function(info) {
+            console.log('eventClick');
+            console.log(info);
         }
+    });
+
+    calendar.render();
+
+    // Menu
+    $("#Logout").click(function() {
+        console.log("Logout");
+        $.post("ajax/menu.php", "Action=Logout", function(data, status) {
+            console.log("Data: " + data + "\nStatus: " + status);
+            if (data == "OUT") {
+                location.reload();
+            }
+        });
     });
 });
